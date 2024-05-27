91c204c1dcf8952793740ce44a6bd9d941820307
João <110414160+joaomanuelmota@users.noreply.github.com>
2024-05-27 16:58:44 +0100

    Create pset0.py

---
+++ pset0/pset0.py
@@ -0,0 +1,8 @@
+import numpy
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number x: "))
+l = round(numpy.log(x))
+
+print(f"{x} raised to the power of {y} is {x ** y}")
+print(f"Log of x is {l}")
